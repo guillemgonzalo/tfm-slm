3812a933f76e3f7b1b6ccb5e8de26f090dbefed1
moonlightKiR <81652246+moonlightKiR@users.noreply.github.com>
2026-07-03 01:06:15 +0200

    update: solved problem with sharegpt files

--- app/dataset/constants.py
+++ app/dataset/constants.py
@@ -1,6 +1,13 @@
-DATASETS_CONFIG: dict[str, dict[str, str | None]] = {
+DATASETS_CONFIG: dict[str, dict[str, str | list[str] | None]] = {
     "open_assistant": {"path": "OpenAssistant/oasst1", "name": None},
-    "sharegpt": {"path": "anon8231489123/ShareGPT_Vicuna_unfiltered", "name": None},
+    "sharegpt": {
+        "path": "json",
+        "name": None,
+        "data_files": [
+            "https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/HTML_cleaned_raw_dataset/sg_90k_part1_html_cleaned.json",
+            "https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/HTML_cleaned_raw_dataset/sg_90k_part2_html_cleaned.json",
+        ],
+    },
     "alpaca": {"path": "tatsu-lab/alpaca", "name": None},
     "ultrachat": {"path": "stingning/ultrachat", "name": None},
     # The Stack is gated and requires manual approval on Hugging Face.
